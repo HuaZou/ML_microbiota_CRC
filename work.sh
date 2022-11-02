cd9bf862f3a1db64ba255455e92ce05729835940
Hua Zou <zouhua1@outlook.com>
2022-11-02 11:38:18 +0800

    init repo

---
+++ work.sh
@@ -0,0 +1,9 @@
+# https://blog.csdn.net/weixin_45092662/article/details/106470862
+
+jupyter nbconvert --to markdown 01.DataClean.ipynb
+jupyter nbconvert --to markdown 02.ExploratoryDataAnalysis.ipynb
+jupyter nbconvert --to markdown 03.DataPreprocesing.ipynb
+jupyter nbconvert --to markdown 04.DifferentialAnalysis.ipynb
+jupyter nbconvert --to markdown 05.PredictiveModelUsingSVM.ipynb
+jupyter nbconvert --to markdown 06.OptimizingSVMClassifier.ipynb
+jupyter nbconvert --to markdown 07.Comparison_between_different_classifiers.ipynb
